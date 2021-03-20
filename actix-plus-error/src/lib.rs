6067a2c407242f514c69341b0462135daf75292a
john01dav <john01dav@gmail.com>
2021-03-20 14:51:23 -0500

    Added unit tests for actix-plus-error and made the code work, according to said unit tests.

--- actix-plus-error/src/lib.rs
+++ actix-plus-error/src/lib.rs
@@ -1,42 +1,42 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use actix_web::HttpResponse;
-use actix_web::web::BytesMut;
 use actix_web::http::StatusCode;
-use actix_web::body::Body;
 use actix_web::dev::HttpResponseBuilder;
+use std::any::Any;
+
+pub trait DowncastableError: Error + Any {}
+impl<T: ?Sized + Error + Any> DowncastableError for T{}
 
 pub type ResponseResult<T> = Result<T, ResponseError>;
 pub type Response = ResponseResult<HttpResponse>;
 
 #[derive(Debug)]
 pub enum ResponseError {
-    InternalServerError(Box<dyn Error>),
+    InternalServerError(Box<dyn DowncastableError>),
     StatusCodeError{code: StatusCode, message: String}
 }
 
 impl Display for ResponseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), ()> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self{
             ResponseError::InternalServerError(err) => {
-                write!(f, "InternalServerError: {}", err)
+                write!(f, "InternalServerError: {}", err)?
             },
             ResponseError::StatusCodeError {code, message} => {
-                write!("HTTP {} {}", code, message)
+                write!(f, "HTTP {} {} ({})", code.as_u16(), code.as_str(), message)?
             }
         }
         Ok(())
     }
 }
 
-impl Error for ResponseError {}
-
 impl actix_web::ResponseError for ResponseError {
 
     fn status_code(&self) -> StatusCode {
         match self{
             ResponseError::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            ResponseError::StatusCodeError {code, ..} => code
+            ResponseError::StatusCodeError {code, ..} => *code
         }
     }
 
@@ -49,9 +49,12 @@ impl actix_web::ResponseError for ResponseError {
 
 }
 
-impl<T: Error + 'static> From<T> for ResponseError {
+impl<T: DowncastableError + 'static> From<T> for ResponseError {
     fn from(err: T) -> Self {
         Self::InternalServerError(Box::new(err))
     }
 }
+
+#[cfg(test)]
+mod tests;
 
---
+++ actix-plus-error/src/tests.rs
@@ -0,0 +1,35 @@
+use crate::Response;
+use actix_web::HttpResponse;
+use std::fmt::{Display, Formatter};
+use actix_web::http::StatusCode;
+use std::error::Error;
+
+#[derive(Debug)]
+struct TestError;
+impl Display for TestError{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TestError")?;
+        Ok(())
+    }
+}
+impl Error for TestError{}
+
+#[test]
+fn test_wrap_error(){
+    fn test_error(should_error: bool) -> Response{
+        if should_error {
+            Err(TestError)?;
+        }
+        Ok(HttpResponse::new(StatusCode::OK))
+    }
+
+    match test_error(false) {
+        Ok(_) => {},
+        Err(_err) => panic!("Found error type in response where none should exist.")
+    }
+
+    match test_error(true) {
+        Ok(_) => panic!("Found Ok type when Err was expected."),
+        Err(_) => {}
+    }
+}
